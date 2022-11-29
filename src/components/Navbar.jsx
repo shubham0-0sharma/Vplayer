09221f7cb44449be0cf6f64b82e0efeb34b84864
shubham sharma <shubham00sharma0@gmail.com>
2022-11-30 00:03:31 +0530

    Added navBar sidebar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Stack } from "@mui/material";
+import {logo} from "./utils/constant"
+import { Link } from "react-router-dom";
+import {SearchBar} from "../components";
+
+
+const Navbar = () => {
+    return (
+        <Stack direction="row"  justifyContent='space-between' alignItems="center" p={2}
+        sx={{position: 'sticky' , backgroundColor:'#000',top:0}}>
+            {" "}
+            <Link to="/" style={{display:"flex", alignItems:"center"}}>
+                <img src={logo} alt="VPlayer-Logo" height={45} />
+            
+            </Link>
+
+            <SearchBar/>
+        </Stack>
+    );
+};
+
+export default Navbar;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export {default as Navbar } from './Navbar.jsx' 
+export {default as Feed } from './Feed.jsx' 
+export {default as SearchBar } from './SearchBar.jsx' 
+export {default as SearchFeed } from './SearchFeed.jsx' 
+export {default as VideoDetail } from './VideoDetail.jsx' 
+export {default as ChannelDetail } from './ChannelDetail.jsx' 
+export {default as Sidebar } from './Sidebar.jsx' 
