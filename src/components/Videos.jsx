8138a45a5c83f03e95dda94cee2e0ed4be2894ca
shubham sharma <shubham00sharma0@gmail.com>
2022-12-08 19:18:39 +0530

    fixed bugs

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,14 +3,19 @@ import {VideoCard, ChannelCard, Loader} from './index'
 const Videos = ({ videos , direction}) => {
     if (!videos?.length) return <Loader/>
     return (
-        <Stack direction={direction ||"row"} flexWrap = "wrap" gap={2} 
-        justifyContent="center"
+        <Stack
+            direction={direction || "row"}
+            flexWrap="wrap"
+            gap={2}
+            sx={{
+                justifyContent: { xs: "center", sm:'start',md:'start' },
+            }}
         >
             {videos.map((item, idx) => (
                 <Box key={idx}>
                     {item.id.videoId && <VideoCard video={item} />}
-                
-                    {item.id.channelId && <ChannelCard channelDetail={item}  />}
+
+                    {item.id.channelId && <ChannelCard channelDetail={item} />}
                 </Box>
             ))}
         </Stack>
